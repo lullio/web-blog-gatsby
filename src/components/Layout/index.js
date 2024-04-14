8f90af7724305acfb4ad79da651b92ee654015e1
Felipe - PCFELIPE_wsl <felipelullio@gmail.com>
2024-04-14 12:49:15 -0300

    add styled components to project and Layout folder

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Profile from "./Profile"
+import Profile from "../Profile"
 
 const Layout = ({ children }) => {
   return (
