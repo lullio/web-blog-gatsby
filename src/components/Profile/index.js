8f8dcf649aff900f7570bca9b74368d23d15dc3a
Felipe - PCFELIPE_wsl <felipelullio@gmail.com>
2024-04-19 20:34:34 -0300

    add styled components to Profile index.js file

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useStaticQuery, graphql } from "gatsby"
 
 import Avatar from '../Avatar'
+import * as S from './styled'
 
 const Profile = () => {
    const {
@@ -21,12 +22,14 @@ const Profile = () => {
  }
 `)
    return (
-      <div className="Profile-wrapper">
-         <Avatar />
-         <h1>{title}</h1>
-         <h2>{position}</h2>
-         <p>{description}</p>
-      </div>
+      <S.ProfileWrapper>
+         <S.ProfileLink>
+            <Avatar />
+            <S.ProfileAuthor>{title}</S.ProfileAuthor>
+            <S.ProfilePosition>{position}</S.ProfilePosition>
+            <S.ProfilePosition>{description}</S.ProfilePosition>
+         </S.ProfileLink>
+      </S.ProfileWrapper>
    )
 }
 export default Profile
--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -7,29 +7,21 @@ export const ProfileWrapper = styled.section`
    flex-direction: column;
 `
 
-export const ProfileLink = styled(AniLink)`
+export const ProfileLink = styled(Link)`
   color: var(--texts);
   text-decoration: none;
   transition: color 0.5s;
 
-  ${media.lessThan("large")`
-    display: flex;
-    text-align: left;
-  `}
-
   &:hover {
-    color: var(--highlight);
-  }git 
+    color: #16202c;
+  } 
 `
 
 export const ProfileAuthor = styled.h1`
   font-size: 1.6rem;
   margin: 0.5rem auto 1.5rem;
 
-  ${media.lessThan("large")`
-    font-size: 1.2rem;
-    margin: 0 0 0 10px;
-  `}
+
 `
 
 export const ProfilePosition = styled.small`
@@ -37,10 +29,7 @@ export const ProfilePosition = styled.small`
   font-size: 1.2rem;
   font-weight: 300;
 
-  ${media.lessThan("large")`
-    font-size: 0.8rem;
-    margin-top: 0.2rem;
-  `}
+
 `
 
 export const ProfileDescription = styled.p`
@@ -48,6 +37,4 @@ export const ProfileDescription = styled.p`
   font-weight: 300;
   line-height: 1.4;
 
-  ${media.lessThan("large")`
-    display: none;
-  `}
+`
