5dec6c86f6d6aeabdd41ec033b717e08f6c38142
Felipe - PCFELIPE_wsl <felipelullio@gmail.com>
2024-04-20 19:45:58 -0300

    fix query and create pages method
    now you can open the post page, it will open the markdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,7 +26,7 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(`
     {
-      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      allMarkdownRemark{
         edges {
           node {
             fields {
@@ -38,25 +38,8 @@ exports.createPages = ({ graphql, actions }) => {
               date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
               description
               title
-              image
             }
             timeToRead
-          }
-          next {
-            frontmatter {
-              title
-            }
-            fields {
-              slug
-            }
-          }
-          previous {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-            }
           }
         }
       }
@@ -64,7 +47,7 @@ exports.createPages = ({ graphql, actions }) => {
   `).then((result) => {
     const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach(({ node, next, previous }) => {
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve(`./src/templates/blog-post.js`),
@@ -72,25 +55,25 @@ exports.createPages = ({ graphql, actions }) => {
           // Data passed to context is available
           // in page queries as GraphQL variables.
           slug: node.fields.slug,
-          previousPost: next,
-          nextPost: previous,
+          //previousPost: next,
+          //nextPost: previous,
         },
       })
     })
-    const postsPerPage = 6
-    const numPages = Math.ceil(posts.length / postsPerPage)
+    // const postsPerPage = 6
+    // const numPages = Math.ceil(posts.length / postsPerPage)
 
-    Array.from({ length: numPages }).forEach((_, index) => {
-      createPage({
-        path: index === 0 ? `/` : `/page/${index + 1}`,
-        component: path.resolve(`./src/templates/blog-list.js`),
-        context: {
-          limit: postsPerPage,
-          skip: index * postsPerPage,
-          numPages,
-          currentPage: index + 1,
-        },
-      })
-    })
+    // Array.from({ length: numPages }).forEach((_, index) => {
+    //   createPage({
+    //     path: index === 0 ? `/` : `/page/${index + 1}`,
+    //     component: path.resolve(`./src/templates/blog-list.js`),
+    //     context: {
+    //       limit: postsPerPage,
+    //       skip: index * postsPerPage,
+    //       numPages,
+    //       currentPage: index + 1,
+    //     },
+    //   })
+    // })
   })
 }    
