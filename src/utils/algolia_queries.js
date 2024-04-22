3d8273aaac150234cac75aa443ed5e771fe91913
Felipe - PCFELIPE_wsl <felipelullio@gmail.com>
2024-04-21 21:34:08 -0300

    add ALGOLIA COMPONENT - FRONTEND
    add all configuration for frontend

--- src/utils/algolia_queries.js
+++ src/utils/algolia_queries.js
@@ -10,6 +10,7 @@ const postQuery = `{
         }
         frontmatter {
           title
+          background
           category
           date_timestamp: date
           date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
@@ -35,7 +36,7 @@ const queries = [
   {
     query: postQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
-    indexName: process.env.ALGOLIA_INDEX_NAME,
+    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
     settings,
   },
 ]
