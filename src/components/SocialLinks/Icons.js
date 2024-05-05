9d5fa64e512fc994354bab0c0fde50d22ad175dc
Felipe - PCFELIPE_wsl <felipelullio@gmail.com>
2024-05-05 16:41:16 -0300

    add teams icon

--- src/components/SocialLinks/Icons.js
+++ src/components/SocialLinks/Icons.js
@@ -2,20 +2,20 @@ import { Github } from "@styled-icons/boxicons-logos/Github"
 import { Whatsapp } from "@styled-icons/boxicons-logos/Whatsapp"
 import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin"
 import { Youtube } from "@styled-icons/boxicons-logos/Youtube"
-import { Skype } from "@styled-icons/boxicons-logos/Skype"
-import { Email } from "@styled-icons/evaicons-outline/EmailOutline"
+import { Email } from "@styled-icons/material-outlined/Email"
 import { StackOverflow } from "@styled-icons/boxicons-logos/StackOverflow"
-import { Messenger } from "@styled-icons/boxicons-logos/Messenger"
+// import { Messenger } from "@styled-icons/boxicons-logos/Messenger"
+import { MicrosoftTeams } from "@styled-icons/boxicons-logos/MicrosoftTeams"
 
 const Icons = {
   Github,
   Whatsapp,
+  MicrosoftTeams,
   Linkedin,
   Youtube,
-  Skype,
   Email,
   StackOverflow,
-  Messenger
+  // Messenger
 }
 
 export default Icons
--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -8,6 +8,10 @@ const links = [
     url: `https://wa.me/5511991486309?text=Ol%C3%A1%2C%20vim%20atrav%C3%A9s%20do%20seu%20blog.%20Gostaria%20de%20tirar%20uma%20d%C3%BAvida%20com%20voc%C3%AA.`,
   },
   {
+    label: "MicrosoftTeams",
+    url: `https://teams.microsoft.com/l/chat/0/0?users=felipe@lullio.com.br`,
+  },
+  {
     label: "Linkedin",
     url: `https://www.linkedin.com/in/felipe-lullio/`,
   },
@@ -16,14 +20,17 @@ const links = [
     url: `http://youtube.com.br/canaldoLULLINHOs2`,
   },
   {
+    label: "Email",
+    url: `mailto:felipelullio@gmail.com?cc=felipe@lullio.com&bcc=lulliofelipe@gmail.com&subject=Oi%20Vim%20pelo%20Blog&body=Podemos%20conversar`,
+  },
+  {
     label: "StackOverflow",
     url: `https://stackoverflow.com/users/12253274/felipe-lullio?tab=profile`,
   },
-  {
-    label: "Messenger",
-    url: `https://m.me/felipee.lullio`,
-  },
-
+  // {
+  //   label: "Messenger",
+  //   url: `https://m.me/felipee.lullio`,
+  // },
 ]
 
 export default links
