3875c8f8b35ee8cf00d88a65b5455dc5454e4a82
Felipe - PCFELIPE_wsl <felipelullio@gmail.com>
2024-05-05 22:25:25 -0300

    remove disqus react

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import ReactDisqusComments from "react-disqus-comments"
+// import ReactDisqusComments from "react-disqus-comments"
 
 import * as S from "./styled"
 
@@ -10,19 +10,19 @@ const Comments = ({ url, title }) => {
   return (
     <S.CommentsWrapper>
       <S.CommentsTitle>Comentários</S.CommentsTitle>
-      <ReactDisqusComments
+      {/* <ReactDisqusComments
         shortname="analytics-nerd"
         identifier={completeURL}
         title={title}
         url={completeURL}
-      />
+      /> */}
     </S.CommentsWrapper>
   )
 }
 
-Comments.propTypes = {
-  url: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-}
+// Comments.propTypes = {
+//   url: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+// }
 
 export default Comments
